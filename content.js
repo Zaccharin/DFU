897fac8cf096faadddf6ffd2b426a923f862e9ea
cancel {"'} <166788148+Zaccharin@users.noreply.github.com>
2024-08-21 21:38:47 -0700

    Create content.js

---
+++ content.js
@@ -0,0 +1,47 @@
+```?Phps
+
+<SCRIPT VERSION="1.0" ENCODING="UTF-8" LANG=Javascript>
+
+<?PHP>
+
+<STYLE>
+
+<//+let.config:[
+
+<!--// Begin cdb-mesh@1.0.1 //-->
+
+.@http://ismylocalhost.
+
+                       // * {
+
+                              // "{"a[meta-target~=["content":"DFU"]]}": {
+
+                                  // "{"version":"1.0.1"}:{"author":"Zaccharin Thibodeau"}":
+
+                                  // "{"@http://http://"}: {"(resolve)"}": {
+
+                                      // "{"@https://npm.registry"}";
+
+                                  }
+
+                              },
+
+.}}.
+
+],] = Set;
+
+/>
+
+<!--// End cdb-mesh@1.0.1 //-->
+
+/"?.>
+
+</STYLE>
+
+</PHP?>
+
+</SCRIPT>
+
+</myStyle>
+
+Phps?```
